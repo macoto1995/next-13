d8daefa26e0c5dfadc5f7aa7846a03c20940d572
Makoto <adosute07@gmail.com>
2022-11-12 14:46:43 +0900

    dynamic and static

---
+++ app/pokemon-dynamic/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+
+type PokemonListResponse = {
+  count: number
+  next: string
+  results: {
+    name: string
+    url: string
+  }[]
+}
+const getPokemonList = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0', { cache: 'no-store' })
+  const json = await res.json() as PokemonListResponse
+  const pokemons = json.results.map(pokemon => {
+    const id = pokemon.url.split('/').at(-2)
+    const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+    const url = `/pokemon/${id}`
+    return({...pokemon, id, url, imgSrc })
+  })
+  return pokemons
+}
+
+export default async function PokemonIndexPage() {
+  const pokemons = await getPokemonList()
+  return (
+    <>
+      {
+        pokemons.map(pokemon => (
+          <div key={pokemon.id}>
+            <h4>{pokemon.name}</h4> 
+            <Link href={pokemon.url}>{pokemon.name}</Link>
+            <img alt={pokemon.name} src={pokemon.imgSrc} />
+          </div>
+        ))
+      }  
+    </>
+  )
+   
+}
