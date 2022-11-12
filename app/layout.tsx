6ad832f3ba1f7bb6ecb7465a6e249509194e3dae
Makoto <adosute07@gmail.com>
2022-11-12 15:18:24 +0900

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,8 +7,19 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
-      <body>{children}</body>
+      <Head>
+        <title>Next.js 13</title>
+      </Head>
+      <body>
+        <header>
+          <div>
+            <h1>
+             Next 13
+            </h1>
+          </div>  
+        </header>  
+        {children}
+      </body>
     </html>
   )
 }
